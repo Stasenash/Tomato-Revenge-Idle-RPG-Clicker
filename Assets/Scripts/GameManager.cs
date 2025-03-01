0b06ef5ba404260bb619c8968dd6d7a558063dbf
Stasenash <stasenash>
2025-03-01 12:48:32 +0500

    Changed assets for main screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,20 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private ClickButtonController _clickButton;
+    [SerializeField] private Animator _shurikenAnimator;
 
     private void Awake()
     {
-        _clickButton.SubscribeOnClick(ShowClick);
+        _clickButton.SubscribeOnClick(AnimateClick);
     }
 
     private void ShowClick()
     {
         Debug.Log("clicked!");
+    }
+    
+    private void AnimateClick()
+    {
+        _shurikenAnimator.SetTrigger("ShurikenAnimation");
     }
 }
