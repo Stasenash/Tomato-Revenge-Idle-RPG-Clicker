4a134e4578a5f26b588d01f2b3d15de60a0495fe
Stasenash <stasenash>
2025-04-18 21:46:14 +0500

    Fixed bug with pins on map

--- Assets/Scripts/Game/EndLevelSystem.cs
+++ Assets/Scripts/Game/EndLevelSystem.cs
@@ -69,6 +69,7 @@ namespace Game
                 if (progress.CurrentLocation < maxLocationAndLevel.x)
                 {
                     progress.CurrentLocation++;
+                    progress.CurrentLevel = 0;
                 }
                 else
                 {
